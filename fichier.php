d9cb3ffe53f21ed1230e7de2426abe2c8f80e7c7
golem13000 <golem13000@hotmail.fr>
2020-04-07 11:26:28 +0200

    compteur.txt

--- fichier.php
+++ fichier.php
@@ -1,10 +1,22 @@
 <?php
 
-$file = fopen("monfichier.txt","r");
+//$file = fopen("monfichier.txt","r");
 
-var_dump(fgets($file));
+//var_dump(fgets($file));
 
+//fclose($file);
+
+$file=fopen("compteur.txt", "r+");
+
+$pages = fgets($file);
+$pages++;
+fseek($file,0);
+fwrite($file, $pages);
 fclose($file);
+
+echo '<p>Cette page a été vue ' . $pages . ' fois !</p>';
+
+?>
 
 
 
