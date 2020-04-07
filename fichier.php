a9f9dc6f1fc115c189138b39a1aa7782ef653e6d
golem13000 <golem13000@hotmail.fr>
2020-04-07 10:05:57 +0200

    compteur.text

--- fichier.php
+++ fichier.php
@@ -1,8 +1,19 @@
 <?php
 
-$file = fopen("monfichier.txt","c");
+//$file = fopen("monfichier.txt","c");
+
+//var_dump(fgets($file));
+
+$file=fopen("compteur.txt", "c");
+
+$page=["affichage.html", "accueil.html"];
+
+$max= sizeof($page);
+
+fwrite($file, "Le nombre d affichage de pages est de". $max . "pages");
 
 var_dump(fgets($file));
+
 
 
 
