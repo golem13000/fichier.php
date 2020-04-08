234367072696329d1785dd5446d903c87a68cd7f
golem13000 <golem13000@hotmail.fr>
2020-04-08 09:02:51 +0200

    index.csv

--- fichier.php
+++ fichier.php
@@ -6,15 +6,28 @@
 
 //fclose($file);
 
-$file=fopen("compteur.txt", "r+");
+//$file=fopen("compteur.txt", "c+");
 
-$pages = fgets($file);
-$pages++;
-fseek($file,0);
-fwrite($file, $pages);
+//$pages = fgets($file);
+//$pages++;
+//fseek($file,0);
+//fwrite($file, $pages);
+//fclose($file);
+
+//echo '<p> Cette page a été vue ' . $pages . ' fois !</p>';
+
+$liste = array(array("Rose", "12","2","100"),
+array("Golum","15","1","100"),
+array("ROCKY","6","1","100","Guerrier","0"));
+
+
+$file = fopen("index.csv", "c+");
+
+foreach($liste as $tab) {
+fputcsv($file, $tab);
+};
+
 fclose($file);
-
-echo '<p> Cette page a été vue ' . $pages . ' fois !</p>';
 
 ?>
 
